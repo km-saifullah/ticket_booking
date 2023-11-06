5bf6cd919951891b1fd500c7264bcf35f42c1836
km-saifullah <kmsaifullah16@gmail.com>
2023-11-06 10:53:03 +0600

    login logics and  feature and ui done!

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// input fields and buttons
+let usernameValue = document.querySelector(".username");
+let passwordValue = document.querySelector(".password");
+let loginBtn = document.querySelector(".loginBtn");
+let loginError = document.querySelector(".login_error");
+loginError.style.display = "none";
+
+// username and password for login
+let username = "saifullah";
+let password = "123456";
+
+// login button activities
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!usernameValue.value && !passwordValue.value) {
+    alert("Enter Username and Password");
+  } else if (
+    usernameValue.value == "saifullah" &&
+    passwordValue.value == "123456"
+  ) {
+    console.log("Login authorized!");
+  } else {
+    loginError.style.display = "block";
+    loginError.style.color = "red";
+    loginError.innerHTML = "Wrong Username and Password!";
+  }
+});
