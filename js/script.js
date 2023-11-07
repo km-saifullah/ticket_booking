db244b4712cec0a481be699403a73ae150bfc8cf
km-saifullah <kmsaifullah16@gmail.com>
2023-11-07 09:53:02 +0600

    train details for chottogram city added

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ let fromStation = document.querySelector(".fromStation");
 let toStation = document.querySelector(".toStation");
 let date = document.querySelector(".date");
 let ticketQuantity = document.querySelector(".quantity");
-let buyBtn = document.querySelector(".buyBtn");
+let chooseBtn = document.querySelector(".buyBtn");
 let bookingError = document.querySelector(".booking_error");
 
 // main heading
@@ -31,8 +31,51 @@ let buyTicket = () => {
   console.log("quantity", ticketQuantity.value);
 };
 
-// ticket price object
-let priceList = {};
+// train details object
+let trainDetails = [
+  {
+    trainId: Math.floor(Math.random() * 100) + 1,
+    trainName: "Chattala Express",
+    city: "Chottogram",
+    AC_B: 1179,
+    AC_S: 1000,
+    SINGDHA: 656,
+    F_BERTH: 788,
+    F_SEAT: 529,
+    F_CHAIR: 850,
+    S_CHAIR: 345,
+    SHOVAN: 285,
+    SHULOV: 250,
+  },
+  {
+    trainId: Math.floor(Math.random() * 100) + 1,
+    trainName: "Mohanagar Express",
+    city: "Chottogram",
+    AC_B: 1100,
+    AC_S: 900,
+    SINGDHA: 656,
+    F_BERTH: 788,
+    F_SEAT: 520,
+    F_CHAIR: 820,
+    S_CHAIR: 345,
+    SHOVAN: 270,
+    SHULOV: 220,
+  },
+  {
+    trainId: Math.floor(Math.random() * 100) + 1,
+    trainName: "Turna Express",
+    city: "Chottogram",
+    AC_B: 1200,
+    AC_S: 1100,
+    SINGDHA: 680,
+    F_BERTH: 800,
+    F_SEAT: 560,
+    F_CHAIR: 850,
+    S_CHAIR: 350,
+    SHOVAN: 300,
+    SHULOV: 250,
+  },
+];
 
 // hide elements form UI
 displayElements(loginError, "none");
@@ -40,8 +83,8 @@ displayElements(ticketBooking, "none");
 displayElements(bookingError, "none");
 
 // username and password for login
-let username = "saifullah";
-let password = "123456";
+let username = "s";
+let password = "1";
 
 showHeading(`Train Booking Center`);
 
@@ -52,13 +95,13 @@ loginBtn.addEventListener("click", (e) => {
   if (!usernameValue.value && !passwordValue.value) {
     displayElements(loginError, "block");
     loginError.innerHTML = "❓ Enter Username and Password";
-  } else if (usernameValue.value == "saifullah" && passwordValue.value == "123456") {
+  } else if (usernameValue.value == "s" && passwordValue.value == "1") {
     showHeading(`Ticket Booking Preference`);
     console.log("Login authorized!");
     displayElements(loginError, "none");
     displayElements(mainLogin, "none");
     displayElements(ticketBooking, "block");
-    buyBtn.addEventListener("click", (e) => {
+    chooseBtn.addEventListener("click", (e) => {
       e.preventDefault();
       buyTicket();
     });
